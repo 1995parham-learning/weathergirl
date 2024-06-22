8c45bfcf11984a8b2fc958333090d8748fd20593
Parham Alvani <parham.alvani@gmail.com>
2024-06-22 15:25:33 +0000

    feat: update datetime configuration

--- main.py
+++ main.py
@@ -1,19 +1,23 @@
+import datetime
+
 from openmeteopy import OpenMeteo
 from openmeteopy.daily import DailyHistorical
-from openmeteopy.hourly import HourlyHistorical
 from openmeteopy.options import HistoricalOptions
 
 # Latitude, Longitude
 longitude = 51.4475302
 latitude = 35.737952
 
-hourly = HourlyHistorical()
+# Data is available for at least 2 days ago.
 daily = DailyHistorical()
 options = HistoricalOptions(
-    latitude, longitude, start_date="2022-12-31", end_date="2023-02-26"
+    latitude,
+    longitude,
+    start_date=datetime.date.today() - datetime.timedelta(days=2),
+    end_date=datetime.date.today() - datetime.timedelta(days=2),
 )
 
-mgr = OpenMeteo(options, hourly.all(), daily.all())
+mgr = OpenMeteo(options, None, daily.all())
 
 # Download data
 meteo = mgr.get_pandas()
