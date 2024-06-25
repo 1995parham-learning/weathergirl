5746ece13f16d6a277ba076652af7f5ec4660b16
Parham Alvani <parham.alvani@gmail.com>
2024-06-25 06:18:33 +0000

    feat: complete code to download weather in districts

--- main.py
+++ main.py
@@ -1,25 +1,38 @@
 import datetime
+import pathlib
 
+import geopandas as gpd
 from openmeteopy import OpenMeteo
-from openmeteopy.daily import DailyHistorical
+from openmeteopy.hourly import HourlyHistorical
 from openmeteopy.options import HistoricalOptions
 
-# Latitude, Longitude
-longitude = 51.4475302
-latitude = 35.737952
+if __name__ == "__main__":
+    districts: gpd.GeoDataFrame = gpd.read_file("districts.geojson")
 
-# Data is available for at least 2 days ago.
-daily = DailyHistorical()
-options = HistoricalOptions(
-    latitude,
-    longitude,
-    start_date=datetime.date.today() - datetime.timedelta(days=2),
-    end_date=datetime.date.today() - datetime.timedelta(days=2),
-)
+    since = datetime.date(2024, 4, 28)
+    to = datetime.date(2024, 5, 18)
 
-mgr = OpenMeteo(options, None, daily.all())
+    for _, district in districts.iterrows():
+        print(f"Reading weather data for {district.name}")
 
-# Download data
-meteo = mgr.get_pandas()
-print(meteo.columns)
-print(meteo)
+        if pathlib.Path(f"{district.id}.csv").exists:
+            print(f"Weather data for {district.name} is already downloaded")
+            continue
+
+        center = district.geometry.centroid
+
+        longitude = center.x
+        latitude = center.y
+
+        # Data is available for at least 2 days ago.
+        hourly = HourlyHistorical()
+        options = HistoricalOptions(
+            latitude,
+            longitude,
+            start_date=since,
+            end_date=to,
+        )
+
+        mgr = OpenMeteo(options, hourly.all(), None)
+
+        mgr.get_pandas().to_csv(f"{district.id}.csv")
